9a6d7ea9d5b7b69186cc957da2f8983419348e7e
Md Robin <jsdev.robin@gmail.com>
2025-05-15 02:12:58 +0600

    Update App component to improve responsiveness with conditional padding

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ChatInputBox from "./components/chat/ChatInputBox";
 
 const App = () => {
   return (
-    <section className="bg-background flex items-center justify-center py-5">
+    <section className="bg-background flex items-center justify-center lg:py-5">
       <div className="container max-w-lg">
         <div className="shadow-2xl w-full rounded-md h-full">
           <div className="flex flex-col">
