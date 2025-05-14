1c38b1f3e76740ec0ae9e093dd7fb26af7aa4476
Md Robin <jsdev.robin@gmail.com>
2025-05-15 01:48:23 +0600

    Add font styles and update ChatHeader component
    - Added Inter font to CSS variables for consistent typography.
    - Changed text weight to bold for "Conference Meeting" in ChatHeader.
    - Updated avatar images in ChatHeader with new URLs.
    - Adjusted heading levels and text styles for better semantics and appearance.

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -15,7 +15,7 @@ const ChatHeader = () => {
           <Button variant="ghost" className="text-muted-foreground">
             <ArrowLeft />
           </Button>
-          <Text variant="base" weight="medium">
+          <Text variant="base" weight="bold">
             Conferacne Meeting
           </Text>
           <Badge variant="secondary" className="text-muted-foreground">
@@ -25,7 +25,10 @@ const ChatHeader = () => {
         <div className="flex items-center gap-2">
           <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
             <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarImage
+                src="https://scontent.fdac24-2.fna.fbcdn.net/v/t39.30808-1/447547678_2426111187594619_2794787843621496013_n.jpg?stp=c448.0.1152.1153a_cp0_dst-jpg_s40x40_tt6&_nc_cat=105&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeF5bReFzlEZ2hBu8xMjKTHmdEuJQYBcePt0S4lBgFx4-3aVpoYZYp9W8w2dz1tsdFmpE0rmae1e7TBZJMlFRj8c&_nc_ohc=3bjF4WDvo1UQ7kNvwG1bNj3&_nc_oc=AdlwZBmyF5iD-GnKlwlEf-fFUiLgYukVEU19b4SBK45hpo21phSHWQwkxEMOj3Ae-PY&_nc_zt=24&_nc_ht=scontent.fdac24-2.fna&_nc_gid=bloge73fFBgdLy7urwR00g&oh=00_AfIHKZyZJdJXLLEqFCgo831tKs1u7xuDj1Fcet1THNQeCA&oe=682ACEB6"
+                alt="@shadcn"
+              />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <Avatar>
@@ -34,7 +37,7 @@ const ChatHeader = () => {
             </Avatar>
             <Avatar>
               <AvatarImage
-                src="https://github.com/evilrabbit.png"
+                src="https://scontent.fdac24-2.fna.fbcdn.net/v/t39.30808-1/486644520_1822592765202206_874228434924014017_n.jpg?stp=c0.0.853.853a_cp0_dst-jpg_s80x80_tt6&_nc_cat=105&ccb=1-7&_nc_sid=9a2c5d&_nc_eui2=AeHwuCWi-hZurKIUIskWnT7RAlI-GP_r1GACUj4Y_-vUYITqsqwgfRhAt63PV4rkZqivBbZ21zQyo9gcblRK-N5_&_nc_ohc=NtZcid4j-7MQ7kNvwEh1BUM&_nc_oc=Adn-vn9qanqIkYRBYi9InsNCj32eiuNGfPTTlMl1jbzSPiKSGCgZwGQi9SefLPNZDrU&_nc_zt=24&_nc_ht=scontent.fdac24-2.fna&_nc_gid=vIwo69QjFsjpQqsJTFltAw&oh=00_AfK6pValFDSJEexppydXL1DMItZ3hh9UelxOwcXdGdXRlw&oe=682ABB2B"
                 alt="@evilrabbit"
               />
               <AvatarFallback>ER</AvatarFallback>
@@ -45,13 +48,13 @@ const ChatHeader = () => {
           </Badge>
         </div>
       </div>
-      <div className="px-8 pb-4 flex items-center justify-between border-b border-border relative before:absolute before:w-20 before:h-0.5 before:bg-foreground before:-bottom-px before:left-0">
+      <div className="px-8 pb-4 flex items-center justify-between border-b border-border">
         <div className="flex items-center gap-2">
           <div>
-            <Heading as="h4" className="font-bold">
+            <Heading as="h5" className="font-bold">
               6:00
             </Heading>
-            <Text variant="base" textColor="muted">
+            <Text variant="sm" weight="medium" textColor="muted">
               Sat, Dec 11
             </Text>
           </div>
@@ -60,7 +63,7 @@ const ChatHeader = () => {
             <Heading as="h4" className="font-bold">
               7:00
             </Heading>
-            <Text variant="base" textColor="muted">
+            <Text variant="sm" weight="medium" textColor="muted">
               Sat, Dec 11
             </Text>
           </div>
