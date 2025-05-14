c0670dd96ecd4588821078827f1181d83b555bba
Md Robin <jsdev.robin@gmail.com>
2025-05-15 02:39:02 +0600

    fix: update container width in App component for better responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ChatInputBox from "./components/chat/ChatInputBox";
 const App = () => {
   return (
     <section className="bg-background flex items-center justify-center lg:py-5">
-      <div className="container max-w-lg">
+      <div className="container md:max-w-3/5 xl:max-w-2/5">
         <div className="shadow-2xl w-full rounded-md h-full">
           <div className="flex flex-col">
             <ChatHeader />
